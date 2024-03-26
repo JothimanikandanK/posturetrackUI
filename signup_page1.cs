2f0bc0893ea0ffd5b2c3e7fadb5c720ffcc11b91
JothimanikandanK <105299806+JothimanikandanK@users.noreply.github.com>
2024-03-26 17:47:10 +0530

    Add files via upload

---
+++ signup_page1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class signup_page1 : MonoBehaviour
+{
+   public void GoToCharacter()
+    {
+        SceneManager.LoadScene("testing");//scene name
+
+    }
+
+    public void GoToSignup()
+    {
+        SceneManager.LoadScene("testing");//scene name
+
+    }
+
+    public void GoToLogin()
+    {
+        SceneManager.LoadScene("testing");//scene name
+
+    }
+}
